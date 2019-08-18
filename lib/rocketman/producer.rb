cfaf98c604553bccd2e8e86ec133a1c719198660
Edison Yap <edisonywh@gmail.com>
2019-08-18 16:21:55 +0800

    Add Redis as a persistence layer
    By default, the events emitted from your app will be stored in an
    in-memory `Queue`, which will get processed by Rocketman threaded
    workers.
    
    However this also means that if your app dies with events still in your
    job queue, your emitted events which are stored in-memory will be lost.
    
    That is obviously not desirable, so that's why Rocketman (this commit) ships with an
    option to use `Redis` as your backing storage mechanism.

--- lib/rocketman/producer.rb
+++ lib/rocketman/producer.rb
@@ -2,7 +2,7 @@ module Rocketman
   module Producer
     def emit(event, payload = {})
       event = Rocketman::Event.new(event.to_sym, payload)
-      Rocketman::Pool.instance.schedule { event.notify_consumers }
+      Rocketman::Pool.instance.jobs.schedule(event)
     end
   end
 end
